e37176d6807feea864b3febc813cbfb8e11eecc5
Hiten Jain <hitenjain146@gmail.com>
2021-12-25 16:12:12 +0530

    Correct Now

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -18,12 +17,6 @@ app.use(express.json());
 //Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 app.use(xss());
-
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests form this IP, please try again in an hour!',
-});
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
