1426dad80946df6eb06af2a63761f48d396b9823
Hiten Jain <hitenjain146@gmail.com>
2021-12-25 16:00:32 +0530

    DB connection

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
-const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,16 +9,14 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 app.use(express.json());
-app.use(helmet());
-app.use(mongoSanitize());
-app.use(xss());
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests form this IP, please try again in an hour!',
-});
-app.use('/', limiter);
+//Set security http headers
+app.use(helmet());
+//Body parser to read data from body(req.body) can restrict amount of data by app.use(express.json({limit : '10kb'}));
+app.use(express.json());
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
--- server.js
+++ server.js
@@ -11,6 +11,22 @@ process.on('uncaughtException', (err) => {
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Server running on ${port}...`);
