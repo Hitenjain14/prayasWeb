2060ccfbc528feb4028d35a75bf556310fab5646
Hiten Jain <hitenjain146@gmail.com>
2021-12-25 15:38:26 +0530

    Error Handling modules

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests form this IP, please try again in an hour!',
+});
+app.use('/', limiter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
